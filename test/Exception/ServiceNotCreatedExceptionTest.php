9403a3892bcb2b3bfde7b2d1ad10acb9d4ae7c3d
fhein <frank.hein@maxence.de>
2018-02-13 22:22:03 +0100

    Document level docblocks added.

--- test/Exception/ServiceNotCreatedExceptionTest.php
+++ test/Exception/ServiceNotCreatedExceptionTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link      https://github.com/zendframework/zend-servicemanager for the canonical source repository
+ * @copyright Copyright (c) 2018, maxence operations gmbh, Germany
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 
 namespace ZendTest\ServiceManager\Exception;
 

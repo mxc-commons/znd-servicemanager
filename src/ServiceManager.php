0654d53643f7e2be68098708e0ee8de23e7ed886
mxc-commons <frank.hein@maxence.de>
2018-02-14 17:19:54 +0100

    Changed exception creation to named constructor.

--- src/ServiceManager.php
+++ src/ServiceManager.php
@@ -678,10 +678,7 @@ class ServiceManager implements ServiceLocatorInterface
             }
         }
 
-        throw new ServiceNotFoundException(sprintf(
-            'Unable to resolve service "%s" to a factory; are you certain you provided it during configuration?',
-            $name
-        ));
+        throw ServiceNotFoundException::fromUnknownService($name);
     }
 
     /**

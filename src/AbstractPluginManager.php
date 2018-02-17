468f6dd222b95cbb5e5729451f91b1fabbf8a642
fhein <frank.hein@maxence.de>
2018-02-17 03:30:24 +0100

    And last one, I hope. What the f*ck? All breaks introduced by git hopefully fixed now.

--- src/AbstractPluginManager.php
+++ src/AbstractPluginManager.php
@@ -122,6 +122,17 @@ abstract class AbstractPluginManager extends ServiceManager implements PluginMan
     }
 
     /**
+     * Override setService for additional plugin validation.
+     *
+     * {@inheritDoc}
+     */
+    public function setService($name, $service)
+    {
+        $this->validate($service);
+        parent::setService($name, $service);
+    }
+
+    /**
      * {@inheritDoc}
      *
      * @param string $name Service name of plugin to retrieve.

a2ab0f556ec4162f0f37495175bf061f1cbdeaa2
fhein <frank.hein@maxence.de>
2018-02-27 19:01:18 +0100

    Removed obsolete named constructors.

--- src/Exception/ServiceNotCreatedException.php
+++ src/Exception/ServiceNotCreatedException.php
@@ -9,7 +9,6 @@ namespace Zend\ServiceManager\Exception;
 
 use Interop\Container\Exception\ContainerException;
 use RuntimeException as SplRuntimeException;
-use Zend\ServiceManager\Factory\DelegatorFactoryInterface;
 
 /**
  * This exception is thrown when the service locator do not manage to create
@@ -26,25 +25,5 @@ class ServiceNotCreatedException extends SplRuntimeException implements
             $name,
             $exception->getMessage()
         ), (int) $exception->getCode(), $exception);
-    }
-
-    public static function fromInvalidClass($name)
-    {
-        return new self(sprintf(
-            'An invalid delegator factory was registered; resolved to class or function "%s" '
-            . 'which does not exist; please provide a valid function name or class name resolving '
-            . 'to an implementation of %s',
-            $name,
-            DelegatorFactoryInterface::class
-        ));
-    }
-
-    public static function fromInvalidInstance($name)
-    {
-        return new self(sprintf(
-            'A non-callable delegator, "%s", was provided; expected a callable or instance of "%s"',
-            is_object($name) ? get_class($name) : gettype($name),
-            DelegatorFactoryInterface::class
-        ));
     }
 }

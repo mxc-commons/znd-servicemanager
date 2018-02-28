f99eacbab0edcf2e261e22e43f204a6a9c1ed193
fhein <frank.hein@maxence.de>
2018-02-28 03:39:50 +0100

    Finalized benchmark suite.

--- benchmarks/Usage250Bench.php
+++ benchmarks/Usage250Bench.php
@@ -15,7 +15,7 @@ use Zend\ServiceManager\ServiceManager;
 use ZendBench\ServiceManager\BenchAsset\DelegatorFactory;
 
 /**
- * @Revs(100)
+ * @Revs(200)
  * @Iterations(20)
  * @OutputTimeUnit("milliseconds", precision=3)
  * @Warmup(2)
